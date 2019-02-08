cab1afeb3794cf09bfdbe5015acb8edf481ff83d
jessvb <jess@csail.mit.edu>
2019-02-08 16:29:39 -0500

    Add query parameters and prep for App Inventor
    * Add query constants class containing various strings that will cause
    SMAPI things to occur (e.g., login with amazon) in preparation for a
    standalone https webpage that App Inventor redirects to in order to get
    the client id
    * Comment out all buttons and button handlers
    * Change html file to have a new title

---
+++ src/com/appinventor/testsmapijava/client/QueryConstants.java
@@ -0,0 +1,12 @@
+package com.appinventor.testsmapijava.client;
+
+/**
+ * Provides constants for query parameters between MIT App Inventor and this 
+ * standalone https webpage.
+ */
+public final class QueryConstants {
+    /**
+     * Login with Amazon Client ID request.
+     */
+    public static final String LOGIN_WITH_AMAZON = "amazonLogin";
+}
